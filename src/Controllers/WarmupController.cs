16305f9866ec41ca9c2b1adbbc8a28b121797a96
Sagar Thakore <sagart12@gmail.com>
2022-01-23 14:21:36 -0500

    add a warmup controller

---
+++ src/Controllers/WarmupController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Jumbled_API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WarmupController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Ready");
+        }
+    }
+}
